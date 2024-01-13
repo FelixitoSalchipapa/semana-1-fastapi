d2cc0f5440a7d3f09f5c889792c4f013024a1a59
FelixitoSalchipapa <felixitopruebaochoa@gmail.com>
2024-01-13 13:42:05 -0500

    Enviando mi primer mensaje para Github

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+def suma(numero1: int, numero2: str):
+    suma = numero1 + numero2
+
+    if( suma % 2 == 0):
+        print("El número es: ", suma)
+        print("Es par")
+    else:
+        print("El número es: ", suma)
+        print("Es impar")
+        print("Es impar")
+        print("Es impar")
+suma(4, 's')
